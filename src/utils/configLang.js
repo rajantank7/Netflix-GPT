df57361257726c5cd468a2cd01b86f1085321636
rajan tank <rajantank7@gmail.com>
2025-07-04 17:01:26 +0530

    done

--- src/utils/configLang.js
+++ src/utils/configLang.js
@@ -12,5 +12,5 @@ const cofigLang = createSlice({
   },
 });
 
-export const { setLang } = configLang.actions;
-export default configLang.reducer;
+export const { setLang } = cofigLang.actions;
+export default cofigLang.reducer;
