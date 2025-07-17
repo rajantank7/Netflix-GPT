6487704f823e90a3675f95fea4896f847ed61061
rajan tank <rajantank7@gmail.com>
2025-07-17 17:17:19 +0530

    done

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ const MovieList = ({ title, movies }) => {
   return (
     <div>
       <div className="px-6">
-        <h1 className="text-2xl py-2 text-white">{title}</h1>
+        <h1 className="text-xl md:text-2xl py-2 text-white">{title}</h1>
         <div className="flex gap-3 overflow-x-scroll scrollbar-hidden">
           {movies?.map((movie) => (
             <div className="min-w-[150px] overflow-visible ">
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -16,7 +16,9 @@ const VideoTitle = ({ title, overview, movieId }) => {
   return (
     <div className="w-full  md:block aspect-[16/9] pt-[25%] pb-10 px-3 md:px-8 absolute text-white bg-gradient-to-r from-black/75">
       <h1 className="md:text-[40px] text-[22px] font-bold ">{title}</h1>
-      <p className="w-1/4 py-1 text-sm hidden md:block  ">{overview}</p>
+      <p className="w-1/4 max-h-27 py-1 text-sm hidden md:block line-clamp-5">
+        {overview}
+      </p>
       <div className="pt-1 md:pt-3">
         <button
           className="p-1 px-2 md:p-2 md:px-8 text-[10px] md:text-base cursor-pointer bg-white text-black rounded-md  hover:hover:bg-white/80"
